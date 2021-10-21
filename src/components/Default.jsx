420fce736201cc38ffe50a7c1ce66c7c9bf2546d
Carlos <arturorueda88@gmail.com>
2021-10-20 19:27:25 -0500

    error page

---
+++ src/components/Default.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Default extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Pagina no encontrada</h3>
+      </div>
+    );
+  }
+}
